1e0c1491734cb7f8d7a9131a0cd1a610e75f32b0
juan <jejenjuanjose@gmail.com>
2024-05-15 16:41:12 -0500

    se agregan los cambios

---
+++ introduccion/bucles/for/for.js
@@ -0,0 +1,22 @@
+
+const nombres = ["juan","maria","pedro","jose"];
+const bucle = nombres.forEach((nombre) =>{
+    return nombre;
+})
+console.log(bucle);
+
+/*
+1 parametro: desde donde empieza 
+2 parametro: desde donde termina
+3 parametro: secuencia de los numeros
+*/
+const valor = prompt("desde que numeros queres que inicie la secuencia");
+for(let numero = 0; numero<=valor; numero++){
+    console.log(numero);
+}
+
+for(let numero = 0; numero<=100; numero++){
+    console.log(numero);
+}
+
+
--- introduccion/spread/java.js
+++ introduccion/spread/java.js
@@ -1,6 +1,6 @@
 const frutas = ["manzana","pera","piña"];
 const comidaFavorita = ["pizza","sushi", ...frutas];
-console.log(comidaFavorita);
+//console.log(comidaFavorita);
 
 const login ={
     correo: "jejenjuanjose@gmail.com",
@@ -14,9 +14,25 @@ const usuario = {
 }
 
 const registro = (nombre,correo, ...datosAdicionales) =>{
-    console.log(nombre,correo, datosAdicionales);
+    //console.log(nombre,correo, datosAdicionales);
 }
-registro("juan","jejenjuanjose@gmail.com", 28, "españa",)
+//registro("juan","jejenjuanjose@gmail.com", 28, "españa",)
 
 
-º
+const amigos = ["maria","pedro","cesar"];
+const [primero,segundo,terceo] = amigos
+//console.log(primero);
+
+const persona = {
+    nombre: "juan",
+    edad: 18,
+    pais: "mexico",
+}
+
+const {nombre,pais, edad} = persona
+//console.log(nombre,pais,edad);
+
+const dad = ({nombre, edad}) =>{
+    console.log(`${nombre} tiene ${edad} años`);
+}
+dad(persona)
