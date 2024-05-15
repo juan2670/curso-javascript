ce8e346b4b15df2ec9f2ca176917ef7049b66a6e
juan <jejenjuanjose@gmail.com>
2024-05-15 18:07:54 -0500

    se agregan los cambios

--- introduccion/bucles/for/for.js
+++ introduccion/bucles/for/for.js
@@ -1,22 +1,38 @@
-
-const nombres = ["juan","maria","pedro","jose"];
+const nombres = ["juan","maria","pedro","jose",];
 const bucle = nombres.forEach((nombre) =>{
     return nombre;
 })
 console.log(bucle);
 
+
 /*
 1 parametro: desde donde empieza 
 2 parametro: desde donde termina
-3 parametro: secuencia de los numeros
+3 parametro: secuencia de incremento del bucle
 */
+
+
 const valor = prompt("desde que numeros queres que inicie la secuencia");
 for(let numero = 0; numero<=valor; numero++){
     console.log(numero);
 }
-
-for(let numero = 0; numero<=100; numero++){
+/*sentencia BREAK*/
+for(let numero = 0; numero<=2; numero++){
     console.log(numero);
 }
+
+for(let i = 0; i <= nombres.length; i++){
+    if(nombres[i][0] !== "a"){
+        break;
+    }
+    console.log(nombres[i]);
+}
+
+
+
+
+
+
+
 
 
